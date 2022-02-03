bf1590bca5ec25ccfc6fcb221a7b9ddcf8e798b6
Sandeep <sandeepsandy0880@gmail.com>
2022-02-03 11:50:45 +0530

    Initial commit

---
+++ Bank1.java
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    String str= "DDDDKKKKK 🤣";
+
+    cout<<str;
+
+    return 0;
+}
---
+++ st.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+ using namespace std;
+
+int main(){
+    string s1 = "DDDKKKKK🤣";
+    cout << s1;
+    cout << endl;
+
+    return 0;
+}
